36a0cc633ef779ab149fbe3f6eb6bdc0273c6011
Dylan Awalt-Conley <web@dylan.ac>
2021-07-23 18:43:43 -0400

    renamed js task

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,12 @@ const sassCompile =() => gulp.src('src/header.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist'));
 
-const jsBrowserify = () => gulp.src('src/header.js')
+const jsCompile = () => gulp.src('src/header.js')
     .pipe(babel({
         presets: [ '@babel/preset-env' ]
     }))
     .pipe(gulp.dest('dist'));
 
-const buildPackage = gulp.parallel(html, sassCompile, jsBrowserify);
+const buildPackage = gulp.parallel(html, sassCompile, jsCompile);
 
 exports.build = buildPackage;
