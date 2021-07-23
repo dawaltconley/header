f3d27abfeb29c6fb876ce2982ac18c56678f92d3
Dylan Awalt-Conley <web@dylan.ac>
2021-07-23 18:44:39 -0400

    added some more basic styling, fake logo

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,8 @@ const path = require('path');
 const p = (...args) => path.join(__dirname, ...args);
 
 module.exports = eleventyConfig => {
-    eleventyConfig.addPassthroughCopy('dist/header.css');
-    eleventyConfig.addPassthroughCopy('dist/header.js');
+    eleventyConfig.addPassthroughCopy('dist');
+    eleventyConfig.addPassthroughCopy('eleventy/css');
 
     return {
         dir: {
