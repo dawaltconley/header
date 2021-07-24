312e9becb9de8f35b7c418b4a0cf08ce5263e82d
Dylan Awalt-Conley <web@dylan.ac>
2021-07-24 03:14:03 -0400

    use yaml for data file

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const p = (...args) => path.join(__dirname, ...args);
+const yaml = require('js-yaml');
 
 module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy('dist');
     eleventyConfig.addPassthroughCopy('eleventy/css');
+
+    // add YAML support
+    eleventyConfig.addDataExtension('yml', data => yaml.safeLoad(data));
+    eleventyConfig.addDataExtension('yaml', data => yaml.safeLoad(data));
 
     return {
         dir: {
