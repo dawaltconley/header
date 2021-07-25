295b602c4a1c4ea530b382364481899f30bb95c0
Dylan Awalt-Conley <web@dylan.ac>
2021-07-25 19:48:35 -0400

    removed unnecessary njk from dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,6 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass')(require('sass'));
 
-const html = () => gulp.src('src/header.njk')
-    .pipe(gulp.dest('dist'));
-
 const sassCompile = () => gulp.src('src/header.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist'));
@@ -26,7 +23,7 @@ const jsCompile = () => gulp.src('src/*.js')
     }))
     .pipe(gulp.dest('dist'));
 
-const buildPackage = gulp.parallel(html, dotCompile, sassCompile, sassCopy, jsCompile);
+const buildPackage = gulp.parallel(dotCompile, sassCompile, sassCopy, jsCompile);
 
 exports.build = buildPackage;
 
