6615232920b7bcf78d3d02854b2be2b910809c06
Dylan Awalt-Conley <web@dylan.ac>
2022-02-10 00:18:02 -0500

    create fixed header in main script

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
 import CollapsibleMenu from './collapsible-menu.mjs';
+import FixedHeader from './fixed-header.mjs';
+
+let fixedHeader = document.querySelector('[data-fixed-header]');
+
+if (fixedHeader)
+    fixedHeader = new FixedHeader(fixedHeader);
 
 const collapsibleMenus = Array.from(document.querySelectorAll('[data-collapsible-menu]'))
-    .map(e => new CollapsibleMenu(e));
+    .map(e => {
+        const o = new CollapsibleMenu(e);
+        if (fixedHeader && fixedHeader.element === e)
+            fixedHeader.menu = o;
+        return o;
+    });
 
 collapsibleMenus.forEach(function (menu) {
     menu.buttons.open.forEach(function (button) {
@@ -11,4 +22,4 @@ collapsibleMenus.forEach(function (menu) {
     });
 });
 
-export { collapsibleMenus };
+export { fixedHeader, collapsibleMenus };
