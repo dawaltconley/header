b86d8d1f2e8d16334f7fbf9b05af236833a87f8e
Dylan Awalt-Conley <web@dylan.ac>
2022-02-11 12:42:23 -0500

    added jsdoc for constructor

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
 import CollapsibleMenu from './collapsible-menu.mjs';
 import FixedHeader from './fixed-header.mjs';
 
-const constructor = (element, options) => {
+/**
+ * Create a fixed header and collapsible menu from the same element.
+ * @param {Element} element - header parent element
+ * @param {Object} [options]
+ * @param {string} [options.name='header'] - base name for BEM classes
+ * @param {Element} [options.scrollable=document.scrollingElement] - element to monitor for scrolling
+ */
+export default (element, options) => {
     let original = new CollapsibleMenu(element, options);
     let fixed = new FixedHeader(element, options);
     fixed.menu = new CollapsibleMenu(fixed.element, options);
 
     return { original, fixed };
 };
-
-export default constructor;
