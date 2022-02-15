ee6affacdcdcd9d2b31d707d5290b43745204b42
Dylan Awalt-Conley <web@dylan.ac>
2022-02-15 14:58:37 -0500

    new export setup to isolate browser js from node js

---
+++ src/js/browser.mjs
@@ -0,0 +1,6 @@
+import constructor from './constructor.mjs';
+import FixedHeader from './fixed-header.mjs';
+import CollapsibleMenu from './collapsible-menu.mjs';
+
+export default constructor;
+export { FixedHeader, CollapsibleMenu };
