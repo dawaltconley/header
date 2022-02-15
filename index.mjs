40797610373425bd587f70f1e7324cdef1cc94ea
Dylan Awalt-Conley <web@dylan.ac>
2022-02-15 14:03:13 -0500

    fixed class exports

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import constructor from './dist/js/constructor.mjs';
+import FixedHeader from './dist/js/fixed-header.mjs';
+import CollapsibleMenu from './dist/js/collapsible-menu.mjs';
 
 export default constructor;
+export { FixedHeader, CollapsibleMenu };
 export { eleventy, generateHtml } from './index.cjs';
-export { FixedHeader } from './dist/js/fixed-header.mjs';
-export { CollapsibleMenu } from './dist/js/collapsible-menu.mjs';
